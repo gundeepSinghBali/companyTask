e6915b4581ddf91211ff3c5122e1e3a4454541df
needJobCoder <97464694+needJobCoder@users.noreply.github.com>
2023-07-28 11:19:10 +0530

    installed react navtive material drawers

---
+++ App.tsx
@@ -0,0 +1,37 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import Login from './src/screens/Login';
+
+function App(): JSX.Element {
+  return (
+    <SafeAreaView>
+      <Login />
+    </SafeAreaView>
+  );
+}
+
+export default App;
